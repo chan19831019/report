e621117e71fece867f3d97022a5e2ef927117400
詹宗哲 <chan_smith@zhanzongzhede-MacBook-Pro.local>
2019-03-17 15:50:43 +0800

    15 ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ReportFourComponent } from './report-four/report-four.component';
-import { ReportFiveComponent } from './report-five/report-five.component';
-import { ReportSixComponent } from './report-six/report-six.component';
-import { ReportSevenComponent } from './report-seven/report-seven.component';
-import { ReportEightComponent } from './report-eight/report-eight.component';
-import { ReportNineComponent } from './report-nine/report-nine.component';
-import { ReportTenComponent } from './report-ten/report-ten.component';
-import { ReportElevenComponent } from './report-eleven/report-eleven.component';
-import { ReportTwelveComponent } from './report-twelve/report-twelve.component';
-import { ReportThirteenComponent } from './report-thirteen/report-thirteen.component';
-import { Report14Component } from './report14/report14.component';
-import { Report15Component } from './report15/report15.component';
-import { Report16Component } from './report16/report16.component';
 import { Report4Component } from './report4/report4.component';
 import { Report5Component } from './report5/report5.component';
 import { Report6Component } from './report6/report6.component';
@@ -26,23 +12,14 @@ import { Report10Component } from './report10/report10.component';
 import { Report11Component } from './report11/report11.component';
 import { Report12Component } from './report12/report12.component';
 import { Report13Component } from './report13/report13.component';
+import { Report14Component } from './report14/report14.component';
+import { Report15Component } from './report15/report15.component';
+import { Report16Component } from './report16/report16.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ReportFourComponent,
-    ReportFiveComponent,
-    ReportSixComponent,
-    ReportSevenComponent,
-    ReportEightComponent,
-    ReportNineComponent,
-    ReportTenComponent,
-    ReportElevenComponent,
-    ReportTwelveComponent,
-    ReportThirteenComponent,
-    Report14Component,
-    Report15Component,
-    Report16Component,
     Report4Component,
     Report5Component,
     Report6Component,
@@ -52,7 +29,10 @@ import { Report13Component } from './report13/report13.component';
     Report10Component,
     Report11Component,
     Report12Component,
-    Report13Component
+    Report13Component,
+    Report14Component,
+    Report15Component,
+    Report16Component
   ],
   imports: [
     BrowserModule,
